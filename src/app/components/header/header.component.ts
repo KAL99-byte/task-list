f5c709080afacf635a6615820bc6a7ed8502417e
KAL99-byte <danielkalasa99@gmail.com>
2025-03-03 02:33:43 +0300

    route properties addition

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,8 +10,12 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title: string = 'Task Tracker';
+  showAddTask!: boolean ;
+  subscription!: Subscription;
 
-  constructor(){
+  constructor(private uiService: UiService,private router:Router) {
+
+   this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
 
   }
 
@@ -17,6 +23,10 @@ export class HeaderComponent implements OnInit {
       
   }
 onAdd(){
-  console.log('onAdd');
+this.uiService.toggleAddTask();
+}
+
+hasRoute(route: string){
+  return this.router.url === route;
 }
 }
